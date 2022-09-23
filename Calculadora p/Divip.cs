86ba8f25f8ade9abda2d540f7ed9a13cfb0c9e0f
Citlali <citlali6soto7@gmail.com>
2022-09-23 08:13:55 -0600

    Agregar archivos de proyecto.

---
+++ Calculadora p/Divip.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora_p
+{
+    class Divip
+    {
+        private int N1;
+        private int N2;
+        public int num1
+        {
+            get => N1; set => N1 = value;
+        }
+        public int num2
+        {
+            get => N2; set => N2 = value;
+        }
+        public int division()
+        {
+            return (N1 / N2);
+
+        }
+    }
+}
---
+++ Calculadora p/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora_p
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnsumar_Click(object sender, EventArgs e)
+        {
+          
+            Sumap sum = new Sumap();
+            sum.num1 = int.Parse(txt1.Text);
+            sum.num2 = int.Parse(txt2.Text);
+
+            txt3.Text = sum.suma().ToString();
+        }
+
+        private void btnrestar_Click(object sender, EventArgs e)
+        {
+            Restap rest = new Restap();
+            rest.num1 = int.Parse(txt1.Text);
+            rest.num2 = int.Parse(txt2.Text);
+
+            txt3.Text = rest.resta().ToString();
+        }
+
+        private void btnmulti_Click(object sender, EventArgs e)
+        {
+            MultiP mcion = new MultiP();
+            mcion.num1 = int.Parse(txt1.Text);
+            mcion.num2 = int.Parse(txt2.Text);
+
+            txt3.Text = mcion.multi().ToString();
+        }
+
+        private void btndivi_Click(object sender, EventArgs e)
+        {
+            Divip Dcion = new Divip();
+            Dcion.num1 = int.Parse(txt1.Text);
+            Dcion.num2 = int.Parse(txt2.Text);
+
+            txt3.Text = Dcion.division().ToString();
+        }
+    }
+    }
+
